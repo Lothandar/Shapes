37e8bb277e1af421cbd8eed12c2a588fb2d8ccde
Lothandar <guillaume.letuppe96@gmail.com>
2017-10-02 15:12:50 +0100

    Origins
    Basic .Net core ConsoleApp generation

---
+++ App.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace shapes
+{
+    class App
+    {
+        public App()
+        {
+
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello Paolo");
+        }
+    }
+}
